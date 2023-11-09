ff548d978ec82708531028daa498266105f5a76c
Rohan-IIT <rpawar3@hawk.iit.edu>
2023-11-09 17:43:52 -0600

    Traditional switch case

--- hello.java
+++ hello.java
@@ -1,6 +1,21 @@
 class Hello {
     public static void main(String args[]) {
-        int b = 7;
-        System.out.println(b);        
+        int b = 3;
+        System.out.println(b); 
+        
+        switch (b) {
+            case 1:
+                System.out.println("First");
+                break;
+            case 2:
+                System.out.println("Second");
+                break;
+            case 3:
+                System.out.println("Third");
+                break;
+            default:
+                System.out.println("default");
+                break;
+        }       
     }
 }
