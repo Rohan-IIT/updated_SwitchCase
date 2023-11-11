9d2f4ec79a6957076707c5940d2548c59dd2d3a0
Rohan-IIT <rpawar3@hawk.iit.edu>
2023-11-11 17:33:12 -0600

    ethod overloading

--- classes.java
+++ classes.java
@@ -3,10 +3,17 @@ public class classes {
 
         int y = 5;
         int x = 10;
-        // Tyep_of_object object_name = new_keyword_to_give_it_space_on_ram name_of_functionality
+        // Tyep_of_object object_name = new_keyword_to_give_it_space_on_ram
+        // name_of_functionality
         funct obj = new funct();
-        int h = obj.addition(x, y);
+
+        // method overloading
+        int h = obj.addition(x, y, 10);
+        int f = obj.addition(x, y);
+
         System.err.println(h);
+        System.err.println(f);
+
     }
 }
 
@@ -15,5 +22,10 @@ class funct {
     public int addition(int l, int m) {
         System.err.println("Hello");
         return (l + m);
+    }
+
+    public int addition(int l, int m, int x) {
+        System.err.println("Hello 2");
+        return (l + m + x);
     }
 }
